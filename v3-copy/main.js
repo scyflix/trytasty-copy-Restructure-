7689072e53c99081175e5eee0646c0c9e4d2833d
abdulroqibscybud <abdulroqeeboladipo@gmail.com>
2026-01-06 20:40:10 +0100

    try: fix google login on mobile

--- v3-copy/main.js
+++ v3-copy/main.js
@@ -65,18 +65,19 @@ onUserAuthChange((user) => {
   }
 });
 
-window.googleLogin = async () => {
-  try {
-    const result = await googleLogin();
-    window.location.href = "submit.html";
-    alert(`Welcome, ${result.user.displayName}`);
-  } catch (e) {
-    alert(e.message);
-  }
-};
 
 googleBtn.forEach((btn) => {
   btn.addEventListener("click", () => {
+    window.googleLogin = async () => {
+      try {
+        const result = await googleLogin();
+        window.location.href = "submit.html";
+        alert(`Welcome, ${result.user.displayName}`);
+      } catch (e) {
+        alert(e.message);
+      }
+    };
+    
     googleLogin();
   });
 });
